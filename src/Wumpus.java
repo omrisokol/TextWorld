aec4b6a42e9d245cc935ff0b368b1258504f281e
omrisokol <48447232+omrisokol@users.noreply.github.com>
2019-04-25 21:47:22 -0700

    Update Wumpus.java

--- src/Wumpus.java
+++ src/Wumpus.java
@@ -1,1 +1,33 @@
+public class Wumpus extends Creature {
+
+    public Wumpus(String name, String description, Level.Room room) {
+        super(name, description, room);
+    }
+
+    @Override
+    public void act() {
+        Level.Room next = getRoom().getRandomNeighbor();
+        String name = this.getName();
+
+        for(int i = 0; i < getRoom().getNeighborNames().length(); i++) {
+            Level.Room neighboringEnemy = getRoom().getNeighbor(name);
+            for(int j = 0; j < this.getRoom().getNeighborNames().length(); j++) {
+                Level.Room neighbor = this.getRoom().getNeighbor(name);
+
+                if(!neighboringEnemy.equals(neighbor)) {
+                    System.out.println(neighboringEnemy);
+                }
+            }
+        }
+
+        int count = 0;
+        do {
+            next = getRoom().getRandomNeighbor();
+            count++;
+            if(count == 5) {
+                System.out.println(next);
+            }
+        } while (next==this.getRoom());
+    }
+}
 
